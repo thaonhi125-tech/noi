eb7cdfcf8cdf88f93aead66ff7e0115510602c5a
thaonhi125 <thaonhi125@gmail.com>
2026-02-15 01:37:56 +0000

    fix: Remove unused GA import from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import ReactGA from 'react-ga4';
-import './index.css';
-import App from './App.tsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// Initialize Google Analytics with the provided Measurement ID
-const gaMeasurementId = 'G-R0LZL5XTQ0';
-ReactGA.initialize(gaMeasurementId);
-
-// Send a pageview event for the initial load
-ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-);
+  </React.StrictMode>,
+)
